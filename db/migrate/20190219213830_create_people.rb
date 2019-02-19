a8fcb6ac2e991c50ef45f86e5373d2279370c386
Luke Herrmann <herrmannluke@gmail.com>
2019-02-19 14:42:38 -0700

    first commit

---
+++ db/migrate/20190219213830_create_people.rb
@@ -0,0 +1,13 @@
+class CreatePeople < ActiveRecord::Migration[5.2]
+  def change
+    create_table :people do |t|
+      t.integer :age
+      t.string :hair_color
+      t.string :eye_color
+      t.string :gender
+      t.boolean :alive
+
+      t.timestamps
+    end
+  end
+end
